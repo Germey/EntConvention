239c46b20dfde626d6e3944becba9e38568fd364
futrees <cqc@cuiqingcai.com>
2015-11-17 15:18:56 +0800

    static pages

--- index.php
+++ index.php
@@ -2,105 +2,168 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <title>企业年会</title>
 </head>
 <body>
-<?php
-/**
- * Created by PhpStorm.
- * User: Jia
- * Date: 2015/11/8
- * Time: 1:01
- */
-
-require_once('init.php');
-require_once('initsql.php');
-header("Content-Type: text/html;charset=utf-8");
-
-
-//判定是否授权
-$code = '';
-if (isset($_GET["code"])) {
-    $code = $_GET['code'];
-} else {
-    $callback_url = $HOST_URL . 'php/index.php';
-//  $scope='snsapi_base';
-//  $scope='snsapi_userinfo';//需要授权
-    $url = \Pingpp\WxpubOAuth::createOauthUrlForCode($MY_APP_KEY, $callback_url, 'arr+fashioncj', false);
-//    echo $url;
-//    echo $callback_url;
-    header("Location:" . $url);
-    exit();
-}
-
-//判定推广
-$state = 'arr+Main';
-if (isset($_GET['state'])) {
-    $state = $_GET['state'];
-} else {
-
-}
-
-//获取open_id
-$openid = \Pingpp\WxpubOAuth::getOpenid($MY_APP_KEY, $MY_APP_SECRET, $code);
-echo $openid.'<br>';
-//echo "<a href=\"payticket.php?name=陈佳&phone=13489092003&address=济南&kind=1&openid=$openid&state=arr+fashioncj\">购票</a>";
-
-//查询余票&&清除锁定
-
-$nowitme = time() - (45 * 60);
-$nowitme = date("Y-m-d H:i:s", $nowitme);
-$sql = "UPDATE ticket SET state=0 WHERE state=1 AND locktime<'{$nowitme}'";
-$query = mysqli_query($con, $sql);
-if ($query) {
-    $sql = "select kind,count(state) from ticket where state=0 group by kind order by kind";
-    $query=mysqli_query($con,$sql);
-    while($row=mysqli_fetch_array($query)){
-        echo $row['kind'] . " " . $row['count(state)'];
-        echo "<br />";
-    }
-} else {
-    echo "FAIL TO CONNECT DATEBASE!";
-    mysqli_close($con);
-    exit();
-}
-
-//$query=mysqli_query($con,$sql);
-
-
-mysqli_close($con);
-?>
-<br>
-<a href="javascript:void(0);" onclick="wap_pay()">购票测试</a>
-<script src="lib/pingpp.js" type="text/javascript"></script>
-<script>
-    function wap_pay() {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "payticket.php?name=陈佳&phone=13489092003&address=济南&kind=1&openid=<?php echo $openid?>&state=arr+fashioncj", true);
-        xhr.send(null);
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                alert(xhr.responseText);
-                var msg=xhr.responseText;
-                var jsons=eval("("+msg+")");
-                //alert(jsons.message);
-                pingpp.createPayment(jsons.charge, function (result, err) {
-                    if (result == "success") {
-                        // 只有微信公众账号 wx_pub 支付成功的结果会在这里返回，其他的 wap 支付结果都是在 extra 中对应的 URL 跳转。
-                        alert("success");
-                    } else if (result == "fail") {
-                        // charge 不正确或者微信公众账号支付失败时会在此处返回
-                        alert("创建订单失败或付款失败，请重试");
-                    } else if (result == "cancel") {
-                        // 微信公众账号支付取消支付
-                        alert("cancel");
-                    }
-                });
-            }
-        }
-    }
-</script>
+    <div class="wrap">
+        <div class="bg">
+            <img src="images/bg.png">
+        </div>
+        <div id="nav" style="display:none">
+            <div class="btns">
+                <a href="#intro" class="btn intro">大会介绍</a>
+                <a href="#guest" class="btn guest">来宾介绍</a>
+                <a href="#letter" class="btn letter">给你的信</a>
+            </div>
+            <div class="bottoms">
+                <div class="row">
+                    <div class="col-xs-6 col">
+                        <a href="#intro" class="btn">点击购票</a>
+                    </div>
+                    <div class="col-xs-6 col">
+                        <img src="images/index_logo.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="intro" class="con" style="display:none">
+            <div class="title">
+                <div class="name pull-left">
+                    <span>大会介绍</span>
+                </div>
+                <div class="close pull-right">
+                    <span>x</span>
+                </div>
+            </div>
+            <div class="content">
+                <div class="top-tag">
+                    <span>导读</span>
+                </div>
+                <div class="details">
+                    <img src="images/intro/img1.png">
+                    <p>2015年是“十二五”收官之年，也是“十三五”谋局之年，更是中国经济转变发展模式进入新常态后的变局之年。决胜未来—2015中国企业家年会在此背景下隆重召开，旨在就宏观形势和热点议题展开深入研讨，帮助企业了解更多全球及国内宏观经济环境及政策走向等问题，协助企业准确研判市场变化，承上启下制定发展规划，做好企业决策。
+2015—中国企业家年会，将邀请部委领导、企业领袖、商界大佬及知名专家现场做精彩分享，相关会议成果将见诸于公众媒体，形成舆论最强音。</p>
+                    <img src="images/intro/img2.png">
+                </div>
+            </div>
+        </div>
+        <div id="focus" class="con" style="display:none">
+            <div class="title">
+                <div class="name pull-left">
+                    <span>大会介绍</span>
+                </div>
+                <div class="close pull-right">
+                    <span>x</span>
+                </div>
+            </div>
+            <div class="content">
+                <div class="top-tag">
+                    <span>会议热点</span>
+                </div>
+                <div class="details">
+                    <h2>资本</h2>
+                    <img src="images/focus/img1.png">
+                    <p>追求高速成长的企业都会在10年内采取积极向外扩张的大动作，例如外延增长，开展新业务，或者进行大的变革，这些都需要借助资本的力量。对企业来说，重大变革需要战略革命，也需要对接资本市场进行融资渠道的开拓。那么当机会来了，企业该如何应对兼并收购的机会？如何为直接融资开通渠道？未来企业兼并重组之后的股权结构如何设计？又如何保证创始股东的利益?</p>
+                    <h2>互联网+</h2>
+                    <p>“互联网+”和中国资本市场正在成为中国经济转型最重要的两个引擎。2015年两会，“互联网+”首次写入了《政府工作报告》，成为国家战略的一部分。国家要推动移动互联网、云计算</p>
+                </div>
+            </div>
+        </div>
+        <div id="guest" class="con" style="display:none">
+            <div class="title">
+                <div class="name pull-left">
+                    <span>来宾介绍</span>
+                </div>
+                <div class="close pull-right">
+                    <span>x</span>
+                </div>
+            </div>
+            <div class="content">
+                <div class="row">
+                    <div class="col-xs-6 col">
+                        <div class="item">
+                            <img src="images/guest/img1.png">
+                            <div class="name">
+                                蒋树声
+                            </div>
+                            <div class="brief">
+                                原人大常委会委员长
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col">
+                        <div class="item">
+                            <img src="images/guest/img2.png">
+                            <div class="name">
+                                王忠禹
+                            </div>
+                            <div class="brief">
+                                原政协副主席中国企业联合会会长
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-6 col">
+                        <div class="item">
+                            <img src="images/guest/img3.png">
+                            <div class="name">
+                                龙永图
+                            </div>
+                            <div class="brief">
+                                原外经贸副部长<br>现任全球CEO发展大会联合主席
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col">
+                        <div class="item">
+                            <img src="images/guest/img4.png">
+                            <div class="name">
+                                周禹鹏
+                            </div>
+                            <div class="brief">
+                                前上海市副市长<br>现代服务业联合会会长
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="seat" class="con">
+            <div class="title">
+                <div class="name pull-left">
+                    <span>购票信息</span>
+                </div>
+                <div class="close pull-right">
+                    <span>x</span>
+                </div>
+            </div>
+            <div class="content">
+                <div class="top-tag">
+                    <span>坐席示意图</span>
+                </div>
+                <div class="details">
+                    <img src="images/seat/img1.png">
+                    <div class="text-center">
+                        <a href="" class="btn">立即购票</a>
+                    </div>
+                    <p><b>座位介绍：</b>座位分为五种，分别为私董席、钻石席、铂金席、白金席、嘉宾席</p>
+            </div>
+        </div>
+        <div id="buy" class="con">
+         </div>
+        <div id="confirm">
+        </div>
+        <div id="success">
+        </div>
+    </div>
+    <script src="js/jquery-1.9.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 </html>
 
